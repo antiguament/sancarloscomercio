6d180732e762ba5a48c6062b3e5030c81fcaa613
antiguament <antiguament@gmail.com>
2025-02-18 22:33:41 -0500

    segundo commit

--- resources/views/content/contenido1.blade.php
+++ resources/views/content/contenido1.blade.php
@@ -4,7 +4,7 @@
 
 
 
- <div class="grid grid-cols-2 md:grid-cols-4 gap-4 mt-24">
+ <div class="grid grid-cols-2 md:grid-cols-4 gap-4 mt-24 bg-blue-100">
   <div class="grid gap-4">
       <div>
           <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt="">
