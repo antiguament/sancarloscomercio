5949bad356c8cd3e5d02cd50e1080821452c5a9d
antiguament <antiguament@gmail.com>
2025-02-21 00:56:52 -0500

    sancarloscomercio1

--- resources/views/content/contenido1.blade.php
+++ resources/views/content/contenido1.blade.php
@@ -4,50 +4,78 @@
 
 
 
- <div class="grid grid-cols-2 md:grid-cols-4 gap-4 mt-24 bg-blue-100">
-  <div class="grid gap-4">
-    hola mundo nuevamente
-      <div>
-          <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt="">
-      </div>
-      <div>
-          <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt="">
-      </div>
-      <div>
-          <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt="">
-      </div>
-  </div>
-  <div class="grid gap-4">
-      <div>
-          <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="">
-      </div>
-      <div>
-          <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt="">
-      </div>
-      <div>
-          <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg" alt="">
-      </div>
-  </div>
-  <div class="grid gap-4">
-      <div>
-          <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" alt="">
-      </div>
-      <div>
-          <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg" alt="">
-      </div>
-      <div>
-          <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" alt="">
-      </div>
-  </div>
-  <div class="grid gap-4">
-      <div>
-          <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg" alt="">
-      </div>
-      <div>
-          <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg" alt="">
-      </div>
-      <div>
-          <img class="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg" alt="">
-      </div>
-  </div>
+ <div class="pt-8 pb-8">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mx-auto max-w-7xl">
+        <!-- Primer elemento -->
+        <div class="rounded-xl shadow-md overflow-hidden relative">
+            <img src="administracion/adm_categoria_r_master/seccion/imagenesr/sabores_1.png" alt="" class="w-full h-52 object-cover">
+            <a href="https://www.saboresdelrolo.com" class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <button class="font-saintmartin text-orange-500 text-3xl px-8 py-3 rounded-lg bg-transparent border border-transparent shadow-md hover:text-white hover:border-orange-500 hover:shadow-lg hover:text-2xl">Saboresdelrolo.com</button>
+            </a>
+        </div>
+
+        <!-- Segundo elemento -->
+        <div class="rounded-xl shadow-md overflow-hidden relative">
+            <img src="administracion/adm_categoria_r_master/seccion/imagenesr/imperio_1.png" alt="" class="w-full h-52 object-cover">
+            <a href="https://imperioelite.comerciosancarlos.com" class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <button class="font-saintmartin text-orange-500 text-3xl px-8 py-3 rounded-lg bg-transparent border border-transparent shadow-md hover:text-white hover:border-orange-500 hover:shadow-lg hover:text-2xl">Imperio Elite</button>
+            </a>
+        </div>
+
+        <!-- Tercer elemento -->
+        <div class="rounded-xl shadow-md overflow-hidden relative">
+            <img src="administracion/adm_categoria_r_master/seccion/imagenesr/bendito_pan1.png" alt="" class="w-full h-52 object-cover">
+            <a href="https://benditopansancarlos.com" class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <button class="font-saintmartin text-orange-500 text-3xl px-8 py-3 rounded-lg bg-transparent border border-transparent shadow-md hover:text-white hover:border-orange-500 hover:shadow-lg hover:text-2xl">Bendito PAN San Carlos</button>
+            </a>
+        </div>
+
+        <!-- Cuarto elemento -->
+        <div class="rounded-xl shadow-md overflow-hidden relative">
+            <img src="administracion/adm_categoria_r_master/seccion/imagenesr/turismo_1.png" alt="" class="w-full h-52 object-cover">
+            <a href="https://turismo.colombiaevolucion.com" class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <button class="font-saintmartin text-orange-500 text-3xl px-8 py-3 rounded-lg bg-transparent border border-transparent shadow-md hover:text-white hover:border-orange-500 hover:shadow-lg hover:text-2xl">Turismo</button>
+            </a>
+        </div>
+
+        <!-- Quinto elemento -->
+        <div class="rounded-xl shadow-md overflow-hidden relative">
+            <img src="administracion/adm_categoria_r_master/seccion/imagenesr/interbarrial1.png" alt="" class="w-full h-52 object-cover">
+            <a href="https://interbarrial.comerciosancarlos.com" class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <button class="font-saintmartin text-orange-500 text-3xl px-8 py-3 rounded-lg bg-transparent border border-transparent shadow-md hover:text-white hover:border-orange-500 hover:shadow-lg hover:text-2xl">Interbarrial</button>
+            </a>
+        </div>
+
+        <!-- Sexto elemento -->
+        <div class="rounded-xl shadow-md overflow-hidden relative">
+            <img src="administracion/adm_categoria_r_master/seccion/imagenesr/esculturas.png" alt="" class="w-full h-52 object-cover">
+            <a href="https://diseñodeesculturassk.com" class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <button class="font-saintmartin text-orange-500 text-3xl px-8 py-3 rounded-lg bg-transparent border border-transparent shadow-md hover:text-white hover:border-orange-500 hover:shadow-lg hover:text-2xl">Diseño de Esculturas</button>
+            </a>
+        </div>
+
+        <!-- Séptimo elemento -->
+        <div class="rounded-xl shadow-md overflow-hidden relative">
+            <img src="administracion/adm_categoria_r_master/seccion/imagenesr/constructora.png" alt="" class="w-full h-52 object-cover">
+            <a href="https://constructora.colombiaevolucion.com" class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <button class="font-saintmartin text-orange-500 text-3xl px-8 py-3 rounded-lg bg-transparent border border-transparent shadow-md hover:text-white hover:border-orange-500 hover:shadow-lg hover:text-2xl">Constructora</button>
+            </a>
+        </div>
+
+        <!-- Octavo elemento -->
+        <div class="rounded-xl shadow-md overflow-hidden relative">
+            <img src="administracion/adm_categoria_r_master/seccion/imagenesr/rapialitas.png" alt="" class="w-full h-52 object-cover">
+            <a href="https://rapialitas.comerciosancarlos.com" class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <button class="font-saintmartin text-orange-500 text-3xl px-8 py-3 rounded-lg bg-transparent border border-transparent shadow-md hover:text-white hover:border-orange-500 hover:shadow-lg hover:text-2xl">Rapi ALitas</button>
+            </a>
+        </div>
+
+        <!-- Noveno elemento -->
+        <div class="rounded-xl shadow-md overflow-hidden relative">
+            <img src="administracion/adm_categoria_r_master/seccion/imagenesr/tienda.png" alt="" class="w-full h-52 object-cover">
+            <a href="#" class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <button class="font-saintmartin text-orange-500 text-3xl px-8 py-3 rounded-lg bg-transparent border border-transparent shadow-md hover:text-white hover:border-orange-500 hover:shadow-lg hover:text-2xl">Tienda</button>
+            </a>
+        </div>
+    </div>
 </div>
